90df6d3e237636ebbb019289ab7345c19dbfb874
ruction <benjaminwoehrl@googlemail.com>
2014-12-25 22:37:37 +0100

    Implemented a basic server/client connection.

--- common/common.pro
+++ common/common.pro
@@ -1,13 +1,17 @@
 TEMPLATE = lib
 
 CONFIG += staticlib
-QT += qml quick
+QT += qml quick network
 
 SOURCES += battleships.cpp \
     board.cpp \
     ship.cpp \
+    client.cpp \
+    server.cpp
 
 HEADERS += \
     battleships.h \
     board.h \
     ship.h \
+    client.h \
+    server.h
--- gui/main.cpp
+++ gui/main.cpp
@@ -6,6 +6,8 @@
 #include <board.h>
 #include <ship.h>
 
+#include "client.h"
+#include "server.h"
 
 int main(int argc, char* argv[])
 {
@@ -14,6 +16,10 @@ int main(int argc, char* argv[])
     qmlRegisterType<Board>("Battleships", 1, 0, "Board");
     qmlRegisterType<Ship>("Battleships", 1, 0, "Ship");
     QQmlApplicationEngine engine(QUrl("qrc:/StartScreen.qml"));
+
+//    Server server;
+    Client client;
+    client.start("127.0.0.1", 8888);
 
     return app.exec();
 }
